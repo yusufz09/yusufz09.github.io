e6d084a0e9f06cf5f0433f3739c769617927cec2
yusufz09 <67047353+yusufz09@users.noreply.github.com>
2022-09-13 09:56:05 +0700

    Delete assets directory

--- assets/js/campaign.js
+++
@@ -1,7 +0,0 @@
-const closeButton = document.getElementById("closePopup");
-const popupAds = document.getElementById("popupAds");
-
-closeButton.addEventListener("click", hidePopup);
-function hidePopup() {
-    popupAds.style.display = "none";
-}
